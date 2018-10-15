af672fb9d2a3d385e704bc3bb1b8a9e2103a9a04
numirias <numirias@users.noreply.github.com>
2018-10-16 00:55:28 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='ctf-announcement',
     packages=['ctf_announcement'],
-    version='0.2',
+    version='0.3',
     author='numirias',
     author_email='numirias@users.noreply.github.com',
     url='https://github.com/secse-ctf/announcement',
